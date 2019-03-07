334521ee3becc53baacf46670f4978add4a267b8
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-03-07 09:41:20 +0100

    feat: add refactored version with native iOS and Android modules

---
+++ ios/Classes/TiNotificationbannerModule.h
@@ -0,0 +1,16 @@
+/**
+ * titanium-notification-banner
+ *
+ * Created by Your Name
+ * Copyright (c) 2018 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@import BRYXBanner;
+
+@interface TiNotificationbannerModule : TiModule
+
+- (void)show:(id)args;
+
+@end
---
+++ ios/Classes/TiNotificationbannerModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiNotificationbannerModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TiNotificationbannerModuleAssets
+
+- (NSData *)moduleAsset
+{
+  
+
+  return nil;
+}
+
+- (NSData *)resolveModuleAsset:(NSString *)path
+{
+  
+
+  return nil;
+}
+
+@end
