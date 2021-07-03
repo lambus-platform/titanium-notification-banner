e4be9ee401eabffb269dda9f6edfebdb6119d611
hansemannn <h.knoechel@lambus.com>
2021-07-03 13:06:37 +0200

    chore: update iOS

--- ios/Classes/TiNotificationbannerModule.swift
+++ ios/Classes/TiNotificationbannerModule.swift
@@ -23,11 +23,6 @@ class TiNotificationbannerModule: TiModule {
     return "ti.notificationbanner"
   }
 
-  override func startup() {
-    super.startup()
-    debugPrint("[DEBUG] \(self) loaded")
-  }
-
   @objc(show:)
   func show(arguments: Array<Any>?) {
     guard let arguments = arguments, let params = arguments[0] as? [String: Any] else { return }
@@ -36,8 +31,8 @@ class TiNotificationbannerModule: TiModule {
     let subtitle = params["subtitle"] as? String
     let duration = params["duration"] as? Double
     let image = params["image"] as? String
-    let titleColor = params["titleColor"] as? String
-    let backgroundColor = params["backgroundColor"] as? String
+    let titleColor = params["titleColor"]
+    let backgroundColor = params["backgroundColor"]
     let onClickCallback = params["onClick"] as? KrollCallback
     
     let banner = Banner(title: title, subtitle: subtitle, image: TiUtils.toImage(image, proxy: self), backgroundColor: TiUtils.colorValue(backgroundColor)?.color ?? UIColor.clear) {
